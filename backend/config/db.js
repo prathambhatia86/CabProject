843e2ae47baea8bdb770e17385642792a0d88d6b
prathambhatia86 <85384335+prathambhatia86@users.noreply.github.com>
2023-07-02 00:26:44 +0530

    hosted database on the atlas cloud

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
 const logger = require('../logger');
-const mongoURI = 'mongodb://127.0.0.1:27017/CabManager';
+const mongoURI = 'mongodb://127.0.0.1:27017/CabManager'; //for database on local machine
+const atlasURI=`mongodb+srv://cab-api:${process.env.database_password}@cluster0.e2jv8iy.mongodb.net/?retryWrites=true&w=majority`
 const connectToMongo = async () => {
-    mongoose.connect(mongoURI).then(() => {
+    mongoose.connect(atlasURI).then(() => {
         logger.info('Connected to mongoose Success');
+     
     }).catch(err => {
         logger.error("Connection to mongodb failed with error code: " + err.code);
     })
