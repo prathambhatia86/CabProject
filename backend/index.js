16c176341f46be6a8da440443f3a7bbb6556d769
prathambhatia86 <85384335+prathambhatia86@users.noreply.github.com>
2023-07-02 11:43:16 +0530

    disabled content security policy to load external files for development

--- backend/index.js
+++ backend/index.js
@@ -30,7 +30,10 @@ setInterval(() => {
 
 
 //MiddleWares used
-app.use(helmet());
+app.use(helmet({
+    contentSecurityPolicy: false,
+}
+));
 app.use(express.json({ limit: "50mb" }))
 app.use(cors());
 app.use(express.urlencoded({ extended: false, limit: "50mb" }))
